faa4a59d1c16b80ce1642bf8dee5f92fbd8e6c86
MyCom <raychaniago21@gmail.com>
2024-04-27 03:25:28 -0700

    end

---
+++ app/src/main/java/com/example/utsperogramanbergerak/ui/Detail.java
@@ -0,0 +1,85 @@
+package com.example.utsperogramanbergerak.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.utsperogramanbergerak.R;
+import com.example.utsperogramanbergerak.data.response.GithubUser;
+import com.example.utsperogramanbergerak.data.retrofit.ApiConfig;
+import com.example.utsperogramanbergerak.data.retrofit.ApiService;
+import com.example.utsperogramanbergerak.databinding.ActivityMainBinding;
+import com.example.utsperogramanbergerak.data.response.GithubUser;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Detail extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String username = extras.getString("username");
+            ApiService apiService = ApiConfig.getApiService();
+            Call<GithubUser> userCall = apiService.getUser(username);
+
+            TextView textView = findViewById(R.id.nama);
+            TextView textView2 = findViewById(R.id.username);
+            TextView textView3 = findViewById(R.id.bio);
+            ImageView imageView = findViewById(R.id.gambar);
+
+            showLoading(true);
+            userCall.enqueue(new Callback<GithubUser>() {
+                @Override
+                public void onResponse(Call<GithubUser> call, Response<GithubUser> response) {
+                    if (response.isSuccessful()){
+                        showLoading(false);
+                        GithubUser user = response.body();
+                        if (user != null){
+                            String name = "Name: " + user.getName();
+                            String usernames = "Username: " + user.getUsername();
+                            String bio = "Bio: " + user.getBio();
+                            String gambar = user.getAvatarUrl();
+
+                            textView.setText(name);
+                            textView2.setText(usernames);
+                            textView3.setText(bio);
+                            Picasso.get().load(gambar).into(imageView);
+                        }else {
+                            Toast.makeText(Detail.this, "Failed to get user data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<GithubUser> call, Throwable t) {
+                    Toast.makeText(Detail.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void showLoading(Boolean isLoading) {
+        if (isLoading) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+}
